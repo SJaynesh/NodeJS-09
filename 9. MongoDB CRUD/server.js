afa99a3823683d2b67b142294d79ddac6852af1b
SJaynesh <jayneshsodvadiya@gmail.com>
2025-12-18 14:30:20 +0530

    CRUD

--- 9. MongoDB CRUD/server.js
+++ 9. MongoDB CRUD/server.js
@@ -10,11 +10,23 @@ app.set('view engine', 'ejs');
 // Middleware
 app.use(express.urlencoded());
 
-app.get('/', (req, res) => {
-    return res.render('form');
+// Table Route
+app.get('/', async (req, res) => {
+
+    // All Book Fetch
+    const allBooks = await Book.find();
+
+    // console.log(allBooks);
+
+    return res.render('table', { allBooks });
 });
 
-// Insert Book
+// Add Book Roate (Form.ejs)
+app.get('/addBookPage', (req, res) => {
+    return res.render('form');
+})
+
+// Insert Book Logic
 app.post('/addBook', async (req, res) => {
     console.log(req.body);
 
@@ -33,13 +45,54 @@ app.post('/addBook', async (req, res) => {
 
     if (bookAdded) {
         console.log("Book inserted Successfully...");
-
-        return res.redirect('/');
     } else {
         console.log("Book insertion failed...");
     }
+    return res.redirect('/');
 
+});
 
+// Edit Route
+app.get('/editBook/:bookId', async (req, res) => {
+    console.log(req.params); // { bookId : 69422b90e1e121e49b97c3b6 }
+
+    // const allBooks = await Book.find();
+
+    // const book = allBooks.find((book) => book.id == req.params.bookId);
+
+    const book = await Book.findById(req.params.bookId);
+
+    console.log(book);
+
+    if (book) {
+        return res.render('edit', { book });
+    } else {
+        return res.redirect('/');
+    }
+});
+
+// Update Book Logic
+app.post('/updateBook', async (req, res) => {
+    console.log(req.body);
+
+    const updatedData = await Book.findByIdAndUpdate(req.body.id, req.body, { new: true });
+
+    console.log("Update : ", updatedData);
+
+    return res.redirect('/');
+});
+
+// Delete Book Logic
+app.get('/deleteBook', async (req, res) => {
+    console.log(req.query);
+
+    const deletedBook = await Book.findByIdAndDelete(req.query.bookId);
+    if (deletedBook) {
+        console.log("Book deleted successfully...");
+    } else {
+        console.log("Book deletion failed...");
+    }
+    return res.redirect('/');
 });
 
 /**
@@ -48,6 +101,35 @@ app.post('/addBook', async (req, res) => {
  *  M - Model => Schema == Structure / Design
  *  V - Views
  *  C - Controllers
+ * 
+ * 
+ *  CRUD -
+ *      C - Create / Insert
+ *      R - Read / Fetch / Retrive
+ *      U - Update
+ *      D - Delete
+ * 
+ *  Mongoose (Model) :
+ *  Insert : create(body)
+ *          body :- object {name: "ABC", age: 12}
+ *          return : Added Single Data  
+ *          not insertion : return null
+ * 
+ *  Fetch : find() 
+ *          return : Array of Objects [{}, {}]
+ *          not inserted : return Empty Array []
+ * 
+ *  Delete : findByIdAndDelete(id)
+ *          return : Delete Single Data
+ *          not deletion : return null
+ * 
+ *  Update : findByIdAndUpdate(id, body , {new : true});
+ *          return : new Updated Data
+ *          not updation : return null 
+ * 
+ *  Single Data Fetch Using Id : findById(id)
+ *          return : Single Data (Match ID)
+ *          not match : return null
  */
 
 app.listen(PORT, (err) => {
