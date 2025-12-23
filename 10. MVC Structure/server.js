739b0c4befeaee944ceab0e533d71bd0cac1b4e3
SJaynesh <jayneshsodvadiya@gmail.com>
2025-12-23 10:13:42 +0530

    MVC

---
+++ 10. MVC Structure/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const app = express();
+const PORT = 8000;
+
+app.set('view engine', 'ejs');
+
+app.use('/', require('./routes/'));
+
+app.listen(PORT, (err) => {
+    if (err) {
+        console.log("Server is not started...", err);
+        return;
+    }
+
+    console.log("Server is started...");
+});
